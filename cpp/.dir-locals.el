fbbbc85f3834c269643c4b1db7476dee0f257b01
MetroWind <chris.corsair@gmail.com>
2020-10-07 23:15:23 -0700

    Move C++ version to its own branch.

--- cpp/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((compile-command . "cd ../build; cmake .. && make")
-       (company-clang-arguments . ("-std=c++2a"))
-       )))
--- cpp/src/utils.h
+++
@@ -1,8 +0,0 @@
-#include "config.h"
-
-#define UNUSED(expr) do { (void)(expr); } while (0)
-
-namespace lorentz
-{
-    constexpr Float PI = 3.141592653589793238462643383279502884;
-}
